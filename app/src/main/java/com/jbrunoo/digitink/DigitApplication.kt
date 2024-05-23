c75fc5a4b94b44a3b0bf91be28efd7297fc5be71
jburnoo <6pizz@naver.com>
2024-05-23 13:27:09 +0900

    feat: 앱 종료 시 모델 해제

--- app/src/main/java/com/jbrunoo/digitink/DigitApplication.kt
+++ app/src/main/java/com/jbrunoo/digitink/DigitApplication.kt
@@ -1,7 +1,16 @@
 package com.jbrunoo.digitink
 
 import android.app.Application
+import com.jbrunoo.digitink.ml.MnistModel
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class DigitApplication: Application()
+class DigitApplication: Application() {
+    @Inject lateinit var model: MnistModel
+
+    override fun onTerminate() {
+        model.close()
+        super.onTerminate()
+    }
+}
