f459925a592b8b935e3dca558b4a019ff8960f3a
jburnoo <6pizz@naver.com>
2024-05-23 13:24:57 +0900

    feat: 이미지 프로세서 주입 받음

--- app/src/main/java/com/jbrunoo/digitink/data/DigitClassifier.kt
+++ app/src/main/java/com/jbrunoo/digitink/data/DigitClassifier.kt
@@ -7,38 +7,29 @@ import androidx.compose.ui.graphics.asAndroidBitmap
 import com.jbrunoo.digitink.domain.Classifier
 import com.jbrunoo.digitink.ml.MnistModel
 import org.tensorflow.lite.DataType
-import org.tensorflow.lite.support.common.ops.NormalizeOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.support.image.ops.ResizeOp
-import org.tensorflow.lite.support.image.ops.TransformToGrayscaleOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 import javax.inject.Inject
 
-class DigitClassifier @Inject constructor(private val model: MnistModel) : Classifier {
+/* 모델은 singleton 생성, application terminate 시 model.close */
+class DigitClassifier @Inject constructor(
+    private val model: MnistModel,
+    private val imgProcessor: ImageProcessor
+) : Classifier {
+    companion object {
+        private const val TAG = "DigitClassifier"
+    }
     override fun classify(imageBitmap: ImageBitmap): Int {
-
 
         var tensorImage = TensorImage(DataType.UINT8)
         tensorImage.load(imageBitmap.asAndroidBitmap().copy(Bitmap.Config.ARGB_8888, false))
-
-        val imageProcessor = ImageProcessor.Builder()
-            .add(ResizeOp(28, 28, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
-            .add(NormalizeOp(0.0f, 255.0f))  // 이 줄 추가 안해서 입력값 달랐음 (1, 150, 150, 3)
-            .add(TransformToGrayscaleOp()) // 회색조 이미지, 라이브러리 tensorflow lite support 필요
-            .build()
-        tensorImage = imageProcessor.process(tensorImage)
+        tensorImage = imgProcessor.process(tensorImage)
 
         val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 28, 28, 1), DataType.FLOAT32)
-
-//        val androidBmp = imageBitmap.asAndroidBitmap()
-//        val resizedBmp = resizeBitmap(androidBmp)
-//        val grayByteBuffer = convertBitmapGrayByteBuffer(resizedBmp)
         inputFeature0.loadBuffer(tensorImage.buffer)
 
-        Log.d("compare buffer shape", "${tensorImage.buffer} / ${inputFeature0.buffer}")
+        Log.d(TAG, "tImgBuf : ${tensorImage.buffer} | inputFeat0Buf ${inputFeature0.buffer}")
 
         // Runs model inference and gets result.
         val outputs = model.process(inputFeature0)
@@ -55,33 +46,7 @@ class DigitClassifier @Inject constructor(private val model: MnistModel) : Class
             }
         }
 
-        model.close()
-
         return maxPos
     }
-
-    private fun resizeBitmap(bitmap: Bitmap) =
-        Bitmap.createScaledBitmap(bitmap, 28, 28, false)
-
-    private fun convertBitmapGrayByteBuffer(bitmap: Bitmap): ByteBuffer {
-        val byteBuffer = ByteBuffer.allocateDirect(bitmap.byteCount)
-        byteBuffer.order(ByteOrder.nativeOrder())
-
-        val pixels = IntArray(bitmap.width * bitmap.height)
-        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
-
-        pixels.forEach { pixel ->
-            val r = pixel shr 16 and 0xFF
-            val g = pixel shr 8 and 0xFF
-            val b = pixel and 0xFF
-
-            val avgPixelValue = (r + g + b) / 3.0f
-            val normalizedPixelValue = avgPixelValue / 255.0f
-
-            byteBuffer.putFloat(normalizedPixelValue)
-        }
-        return byteBuffer
-    }
-
 }
 
--- app/src/main/java/com/jbrunoo/digitink/di/ClassifierModule.kt
+++ app/src/main/java/com/jbrunoo/digitink/di/ClassifierModule.kt
@@ -8,8 +8,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.image.ops.TransformToGrayscaleOp
 import javax.inject.Singleton
 
 @Module
@@ -20,21 +26,31 @@ object ModelModule {
     fun provideModel(@ApplicationContext context: Context): MnistModel {
         return MnistModel.newInstance(context)
     }
+
+    @Provides
+    @Singleton
+    fun provideImgProcessor(): ImageProcessor {
+        return ImageProcessor.Builder()
+            .add(ResizeOp(28, 28, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(NormalizeOp(0.0f, 255.0f))  // 이 줄 추가 안해서 입력값 달랐음 (1, 150, 150, 3)
+            .add(TransformToGrayscaleOp()) // 회색조 이미지, 라이브러리 tensorflow lite support 필요
+            .build()
+    }
 }
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 abstract class ClassifierModule {
 
     @Binds
-    @Singleton
+    @ViewModelScoped
     abstract fun bindClassifier(impl: DigitClassifier): Classifier
 
     companion object {
         @Provides
-        @Singleton
-        fun provideDigitClassifier(model: MnistModel): DigitClassifier {
-            return DigitClassifier(model)
+        @ViewModelScoped
+        fun provideDigitClassifier(model: MnistModel, imgProcessor: ImageProcessor): DigitClassifier {
+            return DigitClassifier(model, imgProcessor)
         }
     }
 }
