0ef432399ec77c1739ac25cefb32408067cc05ce
jburnoo <6pizz@naver.com>
2024-05-28 00:21:14 +0900

    feat: Add scroll
    디폴트 자동 스크롤, 유저 입력 시 스크롤

--- app/src/main/java/com/jbrunoo/digitink/presentation/play/PlayScreen.kt
+++ app/src/main/java/com/jbrunoo/digitink/presentation/play/PlayScreen.kt
@@ -1,20 +1,33 @@
 package com.jbrunoo.digitink.presentation.play
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.pager.VerticalPager
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
@@ -22,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawWithContent
@@ -30,24 +44,31 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.layer.drawLayer
 import androidx.compose.ui.graphics.rememberGraphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import com.jbrunoo.digitink.presentation.play.component.drawCorrectIndicator
 import com.jbrunoo.digitink.presentation.play.component.drawIncorrectIndicator
 import com.jbrunoo.digitink.presentation.play.component.drawUserPaths
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayScreen(
     navController: NavHostController,
     viewModel: PlayViewModel = hiltViewModel()
 ) {
-    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    val uiState = viewModel.playUiState.collectAsStateWithLifecycle()
 
     when (val state = uiState.value) {
         is PlayUiState.Loading -> CircularProgressIndicator()
@@ -74,18 +95,27 @@ fun PlayScreen(
                     text = {}
                 )
             } else {
-                ContentCarousel(
-                    timer = state.timer,
-                    qnaList = state.qnaList,
-                    pathsList = state.pathsList,
-                    onTerminate = { showDialog = true },
-                    onPathsUpdate = {
-                        viewModel.onPathsUpdate(it)
-                    },
-                    onCheckCorrect = { bmp, idx ->
-                        viewModel.onCheckCorrect(bmp, idx)
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(title = {
+                            TimerLayout(timer = state.timer,
+                                onTerminate = { showDialog = true })
+                        })
                     }
-                )
+                ) { paddingValues ->
+                    ContentCarousel(
+                        paddingValues = paddingValues,
+                        qnaList = state.qnaList,
+                        pathsList = state.pathsList,
+                        onTerminate = { showDialog = true },
+                        onPathsUpdate = {
+                            viewModel.onPathsUpdate(it)
+                        },
+                        onCheckDrawResult = { bmp, idx ->
+                            viewModel.onCheckCorrect(bmp, idx)
+                        }
+                    )
+                }
             }
         }
 
@@ -93,32 +123,87 @@ fun PlayScreen(
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ContentCarousel(
-    timer: Int,
+    paddingValues: PaddingValues,
     qnaList: List<QnaState>,
     pathsList: List<List<PathState>>,
     onTerminate: () -> Unit,
     onPathsUpdate: (List<PathState>) -> Unit,
-    onCheckCorrect: (ImageBitmap, Int) -> Unit
+    onCheckDrawResult: (ImageBitmap?, Int) -> Unit
 ) {
-    val pagerState = rememberPagerState(
-        pageCount = { qnaList.size }
-    )
-    Column {
-        TimerLayout(timer, onTerminate = onTerminate)
-        VerticalPager(state = pagerState) { page ->
-            val qnaState = qnaList[page]
-            val paths = if (page < pathsList.size) pathsList[page] else emptyList()
-            Row {
-                QuestionLayout(question = qnaState.question)
+    val configuration = LocalConfiguration.current
+    val topPadding = paddingValues.calculateTopPadding()
+    val itemHeight = (configuration.screenHeightDp.dp - topPadding) / 5
+
+    /* 스크롤 처리 */
+    // 0. 다음 문제가 없으면 스크롤 종료
+    // 1. 5초 동안 유저 입력 받기
+    // 2-1. 유저 입력 없을 시, checkDrawResult == false
+    // 2-2. 유저 입력 시, onCheckDrawResult() 후 5초 딜레이 취소
+    // 3. 0.5초 후 다음 문제 스크롤
+    val listState = rememberLazyListState()
+    var autoScroll by remember { mutableStateOf(false) }
+    var isDraw by remember { mutableStateOf(false) }
+    var delayJob: Job? by remember { mutableStateOf(null) }
+
+    LaunchedEffect(key1 = autoScroll, key2 = isDraw) { // Unit -> userDrawKey: animateScroll 전에 업데이트 방지
+        val nextIdx = listState.firstVisibleItemIndex + 1
+        if (nextIdx >= qnaList.size) {
+            onCheckDrawResult(null, nextIdx - 1)
+            onTerminate()
+        }
+        if(!isDraw) {
+            delayJob = launch {
+                try {
+                    delay(5.seconds)
+                    onCheckDrawResult(null, nextIdx - 1) // checkDrawResult == false
+                    delay(0.5.seconds)
+                    Log.d("autoScroll", "autoScroll")
+                    listState.scrollToItem(nextIdx)
+                } finally {
+                    autoScroll = !autoScroll
+                }
+            }
+        } else {
+            delayJob?.cancelAndJoin() // cancelException 예외 처리해야 하는지
+            delay(0.5.seconds)
+            Log.d("ScrollImmediately", "ScrollImmediately")
+            listState.scrollToItem(nextIdx)
+            isDraw = false
+        }
+    }
+
+
+    LazyColumn(
+        modifier = Modifier
+            .padding(top = paddingValues.calculateTopPadding())
+            .fillMaxSize(),
+        state = listState,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        userScrollEnabled = false
+    ) {
+        items(4) {
+            Spacer(modifier = Modifier.height(itemHeight)) // 화면 높이 이상의 빈 공간
+        }
+        itemsIndexed(qnaList) { idx, qnaState ->
+            val paths = if (idx < pathsList.size) pathsList[idx] else emptyList()
+            val isCurrentQuestion by remember { derivedStateOf{ listState.firstVisibleItemIndex == idx } }
+            Row(
+                modifier = Modifier.height(itemHeight)
+            ) {
+                QuestionLayout(
+                    question = qnaState.question,
+                    borderColor = if (isCurrentQuestion) Color.White else Color.Transparent
+                )
                 DrawDigitLayout(
-                    onCheckCorrect = { bmp ->
-                        onCheckCorrect(bmp, page)
-                    },
                     paths = paths,
-                    isCorrect = qnaState.isCorrect,
+                    checkDrawResult = qnaState.checkDrawResult,
+                    onCheckDrawResult = { bmp ->
+                        isDraw = true
+                        onCheckDrawResult(bmp, idx)
+                    },
                     onPathsUpdate = { onPathsUpdate(it) }
                 )
             }
@@ -139,48 +224,67 @@ fun TimerLayout(timer: Int, onTerminate: () -> Unit) {
             }
         }
     }
-    Text(text = "$ticks")
+    Text(
+        text = "Timer : $ticks",
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+    )
 }
 
 @Composable
-fun QuestionLayout(question: String) {
-    Text(question)
+fun QuestionLayout(question: String, borderColor: Color) {
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .border(1.dp, borderColor)
+    ) {
+        Text(
+            question, style = TextStyle(
+                fontSize = 30.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+        )
+    }
 }
 
 @Composable
 fun DrawDigitLayout(
-    onCheckCorrect: (ImageBitmap) -> Unit,
     paths: List<PathState>,
-    isCorrect: Boolean?,
-    onPathsUpdate: (List<PathState>) -> Unit
+    checkDrawResult: Boolean?,
+    onPathsUpdate: (List<PathState>) -> Unit,
+    onCheckDrawResult: (ImageBitmap) -> Unit,
 ) {
     val userPaths = remember { mutableStateListOf<PathState>() }
+    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
     val coroutineScope = rememberCoroutineScope()
     val graphicsLayer = rememberGraphicsLayer()
 
-    var isDrag by remember { mutableStateOf(false) }
-    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
-
-    if (paths.isNotEmpty()) {
-        Canvas(
-            modifier = Modifier
-                .size(200.dp)
-                .background(Color.Black)
-        ) {
-            drawUserPaths(paths)
-            isCorrect?.let {
-                if (it) drawCorrectIndicator()
-                else drawIncorrectIndicator()
-            }
+    LaunchedEffect(imageBitmap) {
+        imageBitmap?.let {
+            onPathsUpdate(userPaths)
+            onCheckDrawResult(it)
         }
-    } else {
-        Canvas(modifier = Modifier
-            .size(200.dp)
-            .background(Color.Black)
-            .clipToBounds() // 외부까지 그려지는 것 방지, 그려지지만 않을 뿐 드래그 이벤트 중이라 로그는 찍힘
-            .pointerInput(Unit) {
-                detectDragGestures { change, dragAmount ->
-                    isDrag = true
+    }
+
+    val modifier = Modifier
+        .size(200.dp)
+        .background(Color.Black)
+
+    val drawModifier = modifier
+        .clipToBounds() // 외부까지 그려지는 것 방지, 그려지지만 않을 뿐 드래그 이벤트 중이라 로그는 찍힘
+        .pointerInput(Unit) {
+            if (checkDrawResult == null) {
+                detectDragGestures(
+                    onDragEnd = {
+                        coroutineScope.launch {
+                            async {
+                                imageBitmap = graphicsLayer.toImageBitmap()
+                            }.await()
+                        }
+                    }
+                ) { change, dragAmount ->
 //                Log.d("change", "$change, $dragAmount")
                     val path =
                         PathState(
@@ -188,44 +292,25 @@ fun DrawDigitLayout(
                             end = change.position
                         )
                     userPaths.add(path)
-                    isDrag = false
                 }
             }
-            .drawWithContent {
-                graphicsLayer.record {
-                    // draw the contents of the composable into the graphics layer
-                    this@drawWithContent.drawContent()
-                }
-                // draw the graphics layer on the visible canvas
-                drawLayer(graphicsLayer)
-            }
-        ) {
-            drawUserPaths(userPaths)
-            isCorrect?.let {
-                if (it) drawCorrectIndicator()
-                else drawIncorrectIndicator()
-            }
         }
-    }
-    //    LaunchedEffect(isDrag) {
-//        coroutineScope.launch {
-//            val bmp = graphicsLayer.toImageBitmap()
-//            Log.d("bmp", bmp.toString())
-//            delay(5000)
-//            onValidateCorrect(bmp)
-//        }
-//    }
-    Button(onClick = {
-        coroutineScope.launch {
-            async {
-                imageBitmap = graphicsLayer.toImageBitmap()
-            }.await()
-            imageBitmap?.let {
-                onCheckCorrect(it)
-                onPathsUpdate(userPaths)
+        .drawWithContent {
+            graphicsLayer.record {
+                // draw the contents of the composable into the graphics layer
+                this@drawWithContent.drawContent()
             }
+            // draw the graphics layer on the visible canvas
+            drawLayer(graphicsLayer)
         }
-    }) {
-        Text(text = "분류")
+
+    Canvas(
+        modifier = if (paths.isNotEmpty()) modifier else drawModifier
+    ) {
+        drawUserPaths(paths.ifEmpty { userPaths })
+        checkDrawResult?.let {
+            if (it) drawCorrectIndicator()
+            else drawIncorrectIndicator()
+        }
     }
 }
