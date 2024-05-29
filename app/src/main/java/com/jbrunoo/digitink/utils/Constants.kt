28cf3615bda60792b5523a4adf54345d1705929e
jburnoo <6pizz@naver.com>
2024-05-29 21:04:44 +0900

    feat: Add constants

---
+++ app/src/main/java/com/jbrunoo/digitink/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.jbrunoo.digitink.utils
+
+object Constants {
+    const val GAME_RESULT = "gameResult"
+}
+
+enum class GameResultKey(val key: String) {
+    SPEED_GAME_5("speed_game_5"),
+    SPEED_GAME_10("speed_game_10"),
+    SPEED_GAME_15("speed_game_15"),
+    SPEED_GAME_20("speed_game_20")
+}
