026396759e571dd40686815b35ce61fd2a112b17
jburnoo <6pizz@naver.com>
2024-05-29 21:04:29 +0900

    chore: Add dataStore, Remove digitInk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,10 +56,10 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui:1.7.0-alpha07")
+    implementation("androidx.compose.ui:ui:1.7.0-beta01")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
@@ -75,14 +75,14 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // viewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
     // navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    // mlkit - digitInk
-    implementation("com.google.mlkit:digital-ink-recognition:18.1.0")
     // hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
-//    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    // ksp("com.google.dagger:hilt-android-compiler:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    // preferences dataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
