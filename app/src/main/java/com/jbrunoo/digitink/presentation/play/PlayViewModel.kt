35b53f64624e19234a4746a3268d099e8d4a0561
jburnoo <6pizz@naver.com>
2024-05-29 21:03:01 +0900

    fix: 각 flow 분리, limitTime 뷰모델에서 관리 feat: dataStore 추가 중

--- app/src/main/java/com/jbrunoo/digitink/presentation/play/PlayViewModel.kt
+++ app/src/main/java/com/jbrunoo/digitink/presentation/play/PlayViewModel.kt
@@ -1,6 +1,5 @@
 package com.jbrunoo.digitink.presentation.play
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
@@ -13,9 +12,15 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jbrunoo.digitink.domain.Classifier
+import com.jbrunoo.digitink.domain.ResultRepository
+import com.jbrunoo.digitink.utils.GameResultKey
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -25,34 +30,31 @@ import javax.inject.Inject
 @HiltViewModel
 class PlayViewModel @Inject constructor(
     private val classifier: Classifier,
+    private val resultRepository: ResultRepository,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<PlayUiState>(PlayUiState.Loading)
-    private val _pathsList = MutableStateFlow<List<List<PathState>>>(emptyList())
+    private val questionCount: Int =
+        checkNotNull(savedStateHandle["questionCount"]) // 기본적으로 nullable type
 
-    val playUiState = _uiState.combine(_pathsList) { uiState, pathsList ->
-        when (uiState) {
-            is PlayUiState.Success -> {
-                PlayUiState.Success(
-                    timer = uiState.timer,
-                    qnaList = uiState.qnaList,
-                    pathsList = pathsList
-                )
-            }
-
-            else -> uiState
-        }
-    }.stateIn(
-        viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = PlayUiState.Loading
-    )
-
+    private val _limitTime = MutableStateFlow(questionCount * 5000L) // milliseconds
+    val limitTime = _limitTime.asStateFlow()
     var correctCount by mutableIntStateOf(0)
         private set
 
-    private val questionCount: Int =
-        checkNotNull(savedStateHandle["questionCount"]) // 기본적으로 nullable type
+    private val _qnaList = MutableStateFlow<List<QnaState>>(emptyList())
+    private val _pathsList = MutableStateFlow<List<List<PathState>>>(emptyList())
+
+    val uiState: StateFlow<PlayUiState> =
+        combine(_qnaList, _pathsList) { qnaList, pathsList ->
+            PlayUiState.SUCCESS(
+                qnaList = qnaList,
+                pathsList = pathsList
+            )
+        }.stateIn(
+            viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = PlayUiState.LOADING
+        )
 
     init {
         resetGame()
@@ -61,34 +63,38 @@ class PlayViewModel @Inject constructor(
     fun resetGame() {
         generateQnaPairs(questionCount)
         _pathsList.value = emptyList()
+        _limitTime.value = questionCount * 5000L
         correctCount = 0
+        countDownTime()
+    }
+
+    private fun countDownTime() {
+        viewModelScope.launch(Dispatchers.IO) {
+            while(_limitTime.value > 0L) {
+                delay(10L)
+                _limitTime.update { it - 10L }
+//                _limitTime.value -= 10L
+            }
+        }
     }
 
     private fun generateQnaPairs(questionCount: Int) {
         viewModelScope.launch {
-            try {
-                val qnaMutableList = mutableListOf<QnaState>()
-                repeat(questionCount) {
-                    while (true) {
-                        val num1 = (1..9).random()
-                        val num2 = (1..9).random()
-                        val operator = listOf("+", "-").random()
-                        val result = if (operator == "+") num1 + num2 else num1 - num2
-                        if (result in 1..9) { // mnist가 단일 값만 비교되서 단일 결과 값을 가지는 경우만
-                            val qnaState = QnaState("$num1 $operator $num2 =", result, null)
-                            qnaMutableList.add(qnaState)
-                            break // break 빼먹어서 처음으로 OutOfMemoryError 겪음. 모델이랑 그래픽 그리는 부분 문제인 줄..
-                        }
+            val qnaMutableList = mutableListOf<QnaState>()
+            repeat(questionCount) {
+                while (true) {
+                    val num1 = (1..9).random()
+                    val num2 = (1..9).random()
+                    val operator = listOf("+", "-").random()
+                    val result = if (operator == "+") num1 + num2 else num1 - num2
+                    if (result in 1..9) { // mnist가 단일 값만 비교되서 단일 결과 값을 가지는 경우만
+                        val qnaState = QnaState("$num1 $operator $num2 =", result, null)
+                        qnaMutableList.add(qnaState)
+                        break // break 빼먹어서 처음으로 OutOfMemoryError 겪음. 모델이랑 그래픽 그리는 부분 문제인 줄..
                     }
                 }
-                _uiState.value = PlayUiState.Success(
-                    timer = questionCount * 5,
-                    qnaList = qnaMutableList
-                )
-            } catch (e: Exception) {
-                Log.d("generate error", e.message.toString())
-                _uiState.value = PlayUiState.Error("Generate Error")
             }
+            _qnaList.value = qnaMutableList
         }
     }
 
@@ -99,19 +105,14 @@ class PlayViewModel @Inject constructor(
     }
 
     fun onCheckCorrect(bmp: ImageBitmap?, index: Int) {
-        _uiState.update { state ->
-            when (state) {
-                is PlayUiState.Success -> {
-                    val userGuess = classifyBmp(bmp)
-                    val qnaList = state.qnaList.toMutableList()
-                    val qnaState = state.qnaList[index]
-                    val checkDrawResult = userGuess?.let { it == qnaState.answer } ?: false
-                    qnaList[index] = qnaState.copy(checkDrawResult = checkDrawResult)
-                    state.copy(qnaList = qnaList)
-                }
-
-                else -> state
-            }
+        _qnaList.update { qnaList ->
+            val qnaMuList = qnaList.toMutableList()
+            val qnaState = qnaMuList[index]
+            val userGuess = classifyBmp(bmp)
+            val checkDrawResult = userGuess?.let { it == qnaState.answer } ?: false
+            if (checkDrawResult) correctCount++
+            qnaMuList[index] = qnaState.copy(checkDrawResult = checkDrawResult)
+            qnaMuList
         }
     }
 
@@ -123,21 +124,26 @@ class PlayViewModel @Inject constructor(
         }
     }
 
-
-    fun updateCorrectCount() = correctCount++
-
+    fun saveResultEntry() {
+        val key = when (questionCount) {
+            5 -> GameResultKey.SPEED_GAME_5
+            10 -> GameResultKey.SPEED_GAME_10
+            15 -> GameResultKey.SPEED_GAME_15
+            20 -> GameResultKey.SPEED_GAME_20
+            else -> null
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            key?.let { resultRepository.saveResult(key, correctCount.toString()) }
+        }
+    }
 }
 
-
-sealed class PlayUiState {
-    data class Success(
-        val timer: Int,
+sealed interface PlayUiState {
+    data object LOADING: PlayUiState
+    data class SUCCESS(
         val qnaList: List<QnaState>,
-        val pathsList: List<List<PathState>> = emptyList()
-    ) : PlayUiState()
-
-    data object Loading : PlayUiState()
-    data class Error(val message: String) : PlayUiState()
+        val pathsList: List<List<PathState>>
+    ): PlayUiState
 }
 
 data class QnaState(
@@ -153,3 +159,9 @@ data class PathState(
     val alpha: Float = 0.8f,
     val strokeWidth: Dp = 4.dp
 )
+
+sealed class GameResultType(val message: String) {
+    data object SUCCESS : GameResultType("Congratulations") // 절반 이상 정답
+    data object FAILED : GameResultType("Shameful") // 절반 이상 오답
+    data object TIMEOUT : GameResultType("Time-out")
+}
