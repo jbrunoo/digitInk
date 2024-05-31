30e107bdef15d8ce9c0ba211d18e2b921c369c90
jburnoo <6pizz@naver.com>
2024-06-01 00:30:17 +0900

    feat: Add GameResult model

---
+++ app/src/main/java/com/jbrunoo/digitink/domain/model/Result.kt
@@ -0,0 +1,8 @@
+package com.jbrunoo.digitink.domain.model
+
+data class Result(
+    val speedGame5: String = "no-result",
+    val speedGame10: String = "no-result",
+    val speedGame15: String = "no-result",
+    val speedGame20: String = "no-result"
+)
