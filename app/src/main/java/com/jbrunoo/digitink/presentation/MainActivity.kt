e541086c464381d20cc7d18dd40f20a8a7cef2fe
jburnoo <6pizz@naver.com>
2024-06-01 00:28:24 +0900

    move

--- app/src/main/java/com/jbrunoo/digitink/presentation/MainActivity.kt
+++ app/src/main/java/com/jbrunoo/digitink/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jbrunoo.digitink
+package com.jbrunoo.digitink.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,12 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.jbrunoo.digitink.navigation.RootNavHost
 import com.jbrunoo.digitink.ui.theme.DigitInkTheme
 import dagger.hilt.android.AndroidEntryPoint
 
