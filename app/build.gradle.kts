337d763c600d2cff52cfc169a29a56dc8db3e079
jbrunoo <6pizz@naver.com>
2025-03-26 14:50:53 +0900

    chore: version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.jbrunoo.digitink"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.3-alpha02"
+        versionCode = 6
+        versionName = "1.0.1-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
