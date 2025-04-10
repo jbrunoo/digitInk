f60221df29326fc1460b138d735440adda9a84ec
jbrunoo <6pizz@naver.com>
2025-04-10 13:50:11 +0900

    fix: UI 수정(black & white, card UI)

--- app/src/main/java/com/jbrunoo/digitink/presentation/MainActivity.kt
+++ app/src/main/java/com/jbrunoo/digitink/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jbrunoo.digitink.presentation
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -24,6 +25,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
         enableEdgeToEdge()
 
         playGamesManager.apply {
