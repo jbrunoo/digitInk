4237a3546c4f1a1825c70e93bd2288799dae558c
jbrunoo <6pizz@naver.com>
2025-04-10 13:47:49 +0900

    fix: 리더보드 키 변경

--- app/src/main/java/com/jbrunoo/digitink/utils/Constants.kt
+++ app/src/main/java/com/jbrunoo/digitink/utils/Constants.kt
@@ -6,8 +6,8 @@ object Constants {
     const val DATASTORE_KEY_15 = "speed_game_15"
     const val DATASTORE_KEY_20 = "speed_game_20"
 
-    const val LEADERBOARD_KEY_5 = "CgkIppS0iLUcEAIQAQ"
-    const val LEADERBOARD_KEY_10 = "CgkIppS0iLUcEAIQAg"
-    const val LEADERBOARD_KEY_15 = "CgkIppS0iLUcEAIQAw"
-    const val LEADERBOARD_KEY_20 = "CgkIppS0iLUcEAIQBA"
+    const val LEADERBOARD_KEY_5 = "CgkIppS0iLUcEAIQBg"
+    const val LEADERBOARD_KEY_10 = "CgkIppS0iLUcEAIQBw"
+    const val LEADERBOARD_KEY_15 = "CgkIppS0iLUcEAIQCA"
+    const val LEADERBOARD_KEY_20 = "CgkIppS0iLUcEAIQCQ"
 }
