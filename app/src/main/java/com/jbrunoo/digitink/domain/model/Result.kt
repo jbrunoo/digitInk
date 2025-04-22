0e2e2277c3fc94902105b2bb93af34b8efae4c9a
jbrunoo <6pizz@naver.com>
2025-04-22 23:57:51 +0900

    feat: infinite mode 결과 추가

--- app/src/main/java/com/jbrunoo/digitink/domain/model/Result.kt
+++ app/src/main/java/com/jbrunoo/digitink/domain/model/Result.kt
@@ -1,17 +1,19 @@
 package com.jbrunoo.digitink.domain.model
 
 data class Result(
-    val speedGame5: Long = 0,
-    val speedGame10: Long = 0,
-    val speedGame15: Long = 0,
-    val speedGame20: Long = 0,
+    val normalMode5: Long = 0,
+    val normalMode10: Long = 0,
+    val normalMode15: Long = 0,
+    val normalMode20: Long = 0,
+    val infiniteMode: Long = 0,
 ) {
     fun find(count: Int): Double {
         val value = when(count) {
-            5 -> speedGame5
-            10 -> speedGame10
-            15 -> speedGame15
-            else -> speedGame20
+            5 -> normalMode5
+            10 -> normalMode10
+            15 -> normalMode15
+            20 -> normalMode20
+            else -> return infiniteMode.toDouble()
         }
 
         return value / 100.0
