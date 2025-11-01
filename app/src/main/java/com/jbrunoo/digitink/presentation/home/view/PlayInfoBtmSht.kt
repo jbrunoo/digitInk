5e2d7476b94bc01d4e5af364d20652913e640aba
jbrunoo <6pizz@naver.com6pizz@naver.comv6pizz@naver.com6pizz@naver.com6pizz@naver.com6pizz@naver.com>
2025-11-01 18:12:32 +0900

    chore: update dependencies

--- app/src/main/java/com/jbrunoo/digitink/presentation/home/view/PlayInfoBtmSht.kt
+++ app/src/main/java/com/jbrunoo/digitink/presentation/home/view/PlayInfoBtmSht.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -64,7 +63,8 @@ private fun PlayInfoBtmShtPreview() {
         SheetState(
             skipPartiallyExpanded = false,
             initialValue = SheetValue.Expanded,
-            density = LocalDensity.current,
+            positionalThreshold = { 0f },
+            velocityThreshold = { 0f },
             skipHiddenState = false,
         )
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.11.0-rc02" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.firebase.crashlytics") version "3.0.4" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "13.0.0-rc.1" apply false
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.firebase.crashlytics") version "3.0.6" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "13.1.0" apply false
 }
